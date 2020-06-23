e18434b260a92540a9cc30e570f96ac43f9ba899
Adam L <203779+seethroughtrees@users.noreply.github.com>
2020-06-22 23:54:21 -0700

    using event check

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,9 @@ module Main exposing (Model, Msg(..), init, main, update, view)
 
 -- import Html.Attributes exposing ()
 
+import Bitwise exposing (and)
 import Browser
+import Browser.Dom exposing (getElement)
 import Browser.Events exposing (onMouseMove)
 import Color
 import Debug
@@ -10,6 +12,7 @@ import Html exposing (Html, div, h3, i, text)
 import Html.Attributes exposing (style)
 import Json.Decode as Decode
 import Svg.Events
+import Task
 import TypedSvg exposing (circle, polygon, svg)
 import TypedSvg.Attributes exposing (cx, cy, fill, points, r, viewBox)
 import TypedSvg.Types exposing (Paint(..), px)
@@ -22,13 +25,15 @@ import TypedSvg.Types exposing (Paint(..), px)
 type alias Model =
     { ballPosition : Position
     , dragState : DragState
+    , triangleEl : Maybe Browser.Dom.Element
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { ballPosition = Position 300 300
+    ( { ballPosition = Position 0.5 0.5
       , dragState = Released
+      , triangleEl = Nothing
       }
     , Cmd.none
     )
@@ -52,6 +57,17 @@ type DragState
 type Msg
     = UpdatePosition Float Float
     | UpdateDrag DragState
+    | GetTriangle (Result Browser.Dom.Error Browser.Dom.Element)
+
+
+getEl : Cmd Msg
+getEl =
+    Task.attempt GetTriangle (getElement "mainTriangle")
+
+
+sideOfLine : ( Float, Float ) -> ( Float, Float ) -> ( Float, Float ) -> Float
+sideOfLine ( x, y ) ( x1, y1 ) ( x2, y2 ) =
+    (x - x1) * (y2 - y1) - (y - y1) * (x2 - x1)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -60,20 +76,59 @@ update msg model =
         UpdatePosition x y ->
             let
                 newX =
-                    x
+                    case model.triangleEl of
+                        Just { element } ->
+                            (x - element.x) / element.width
+
+                        Nothing ->
+                            x
 
                 newY =
-                    y
+                    case model.triangleEl of
+                        Just { element } ->
+                            let
+                                newValue =
+                                    (y - element.y) / element.height
+                            in
+                            if newValue < 1 then
+                                newValue
+
+                            else
+                                1
+
+                        Nothing ->
+                            y
+
+                isInsideOnLeft =
+                    sideOfLine ( newX, newY ) ( 0, 1 ) ( 0.5, 0 ) < 0
+
+                isInsideOnRight =
+                    sideOfLine ( newX, newY ) ( 1, 1 ) ( 0.5, 0 ) > 0
+
+                isInside =
+                    Debug.log "isInside" (isInsideOnLeft && isInsideOnRight)
             in
             ( { model | ballPosition = Position newX newY }, Cmd.none )
 
         UpdateDrag dragState ->
             case dragState of
                 Pressed ->
-                    ( { model | dragState = dragState }, Cmd.none )
+                    ( { model | dragState = dragState }, getEl )
 
                 Released ->
-                    ( { model | dragState = dragState }, Cmd.none )
+                    Debug.log "Released" ( { model | dragState = dragState }, Cmd.none )
+
+        GetTriangle result ->
+            let
+                a =
+                    Debug.log "result" result
+            in
+            case result of
+                Ok el ->
+                    ( { model | triangleEl = Just el }, Cmd.none )
+
+                Err _ ->
+                    ( model, Cmd.none )
 
 
 
@@ -83,18 +138,38 @@ update msg model =
 
 viewCoordinates : Model -> Html msg
 viewCoordinates model =
+    let
+        widthText =
+            case model.triangleEl of
+                Just el ->
+                    String.fromFloat el.element.width
+
+                Nothing ->
+                    ""
+
+        heightText =
+            case model.triangleEl of
+                Just el ->
+                    String.fromFloat el.element.height
+
+                Nothing ->
+                    ""
+
+        clickedText =
+            if model.dragState == Pressed then
+                "It is clicked"
+
+            else
+                "It is not clicked"
+    in
     div []
         [ h3 [] [ text (String.fromFloat model.ballPosition.x) ]
         , h3 [] [ text (String.fromFloat model.ballPosition.y) ]
-        , h3 []
-            [ text
-                (if model.dragState == Pressed then
-                    "It is clicked"
+        , h3 [] [ text clickedText ]
+        , h3 [] [ text ("height: " ++ widthText) ]
+        , h3 [] [ text ("width: " ++ heightText) ]
 
-                 else
-                    "It is not clicked"
-                )
-            ]
+        -- [ text (String.fromFloat model.triangleEl.h) ]
         ]
 
 
@@ -102,28 +177,31 @@ viewTriangle : Model -> Html Msg
 viewTriangle model =
     let
         w =
-            600
+            100
 
         h =
-            520
+            w * (Basics.sqrt 3 / 2)
+
+        ballRadius =
+            2
     in
     svg
         [ viewBox 0 0 w h
-        , Svg.Events.onMouseUp (UpdateDrag Released)
+        , Html.Attributes.style "border" "1px solid red"
         ]
         [ polygon
             [ points [ ( w / 2, 0 ), ( w, h ), ( 0, h ) ]
             , TypedSvg.Attributes.id "mainTriangle"
             , fill (Paint (Color.rgba 0.8 0.8 0.8 1))
-            , Svg.Events.onMouseOut (UpdateDrag Released)
             ]
             []
         , circle
-            [ cx (px model.ballPosition.x)
-            , cy (px model.ballPosition.y)
-            , r (px 20)
+            [ cx (px (model.ballPosition.x * w))
+            , cy (px (model.ballPosition.y * h))
+            , r (px ballRadius)
             , fill (Paint (Color.rgba 0 0 0 1))
             , Svg.Events.onMouseDown (UpdateDrag Pressed)
+            , Svg.Events.onMouseUp (UpdateDrag Released)
             ]
             []
         ]
@@ -131,7 +209,7 @@ viewTriangle model =
 
 view : Model -> Html Msg
 view model =
-    div [ style "position" "relative" ]
+    div [ style "position" "relative", style "margin" "14rem" ]
         [ viewTriangle model
         , viewCoordinates model
         ]
